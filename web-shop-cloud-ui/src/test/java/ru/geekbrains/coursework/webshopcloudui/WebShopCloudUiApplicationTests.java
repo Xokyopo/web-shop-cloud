63422e732fe149a363f7b5c754c94ba41bd7e439
Alexey <enslaver@list.ru>
2020-11-28 21:23:05 +0300

    created ui module

---
+++ web-shop-cloud-ui/src/test/java/ru/geekbrains/coursework/webshopcloudui/WebShopCloudUiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.coursework.webshopcloudui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopCloudUiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
