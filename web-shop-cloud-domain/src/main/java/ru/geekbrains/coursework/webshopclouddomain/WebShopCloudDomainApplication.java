af33b29635828e7e282c676d83981401bc384c64
Alexey <enslaver@list.ru>
2020-11-28 21:13:17 +0300

    created domain module

---
+++ web-shop-cloud-domain/src/main/java/ru/geekbrains/coursework/webshopclouddomain/WebShopCloudDomainApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.coursework.webshopclouddomain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopCloudDomainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopCloudDomainApplication.class, args);
+	}
+
+}
---
+++ web-shop-cloud-domain/src/test/java/ru/geekbrains/coursework/webshopclouddomain/WebShopCloudDomainApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.coursework.webshopclouddomain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopCloudDomainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
