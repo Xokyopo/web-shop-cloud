40aa433430b02bf3a5c076d9413226f079bf6133
Alexey <enslaver@list.ru>
2020-11-28 21:02:48 +0300

    created security and aop-logging module

---
+++ web-shop-cloud-security-and-logging/src/main/java/ru/geekbrains/coursework/webshopcloudsecurityandlogging/WebShopCloudSecurityAndLoggingApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.coursework.webshopcloudsecurityandlogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopCloudSecurityAndLoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopCloudSecurityAndLoggingApplication.class, args);
+	}
+
+}
