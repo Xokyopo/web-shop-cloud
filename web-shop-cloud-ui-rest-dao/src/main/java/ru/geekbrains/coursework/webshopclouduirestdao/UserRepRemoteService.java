c338752b71f7639dce79bb1701434dfff898e164
Alexey <enslaver@list.ru>
2020-11-29 23:39:20 +0300

    removed redundant method's parameter

--- web-shop-cloud-ui-rest-dao/src/main/java/ru/geekbrains/coursework/webshopclouduirestdao/UserRepRemoteService.java
+++ web-shop-cloud-ui-rest-dao/src/main/java/ru/geekbrains/coursework/webshopclouduirestdao/UserRepRemoteService.java
@@ -3,5 +3,5 @@ package ru.geekbrains.coursework.webshopclouduirestdao;
 import ru.geekbrains.coursework.webshopclouduirestdao.representations.UserRep;
 
 public interface UserRepRemoteService extends ARemoteService<UserRep> {
-    void addNewUser(String login, String password, String rePassword) throws Exception;
+    void addNewUser(String login, String password) throws Exception;
 }
