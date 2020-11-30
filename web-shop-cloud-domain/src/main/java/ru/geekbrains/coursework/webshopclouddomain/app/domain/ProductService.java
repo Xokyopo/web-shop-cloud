75a540abf14b8a4a67ee0a63c5f49efb4a4c07c1
Alexey <enslaver@list.ru>
2020-11-30 16:37:35 +0300

    rewritten because modified extends superclass signature

--- web-shop-cloud-domain/src/main/java/ru/geekbrains/coursework/webshopclouddomain/app/domain/ProductService.java
+++ web-shop-cloud-domain/src/main/java/ru/geekbrains/coursework/webshopclouddomain/app/domain/ProductService.java
@@ -3,7 +3,12 @@ package ru.geekbrains.coursework.webshopclouddomain.app.domain;
 import org.springframework.stereotype.Service;
 import ru.geekbrains.coursework.webshopclouddomain.app.dao.ProductRepository;
 import ru.geekbrains.coursework.webshopclouddomain.app.domain.entities.Product;
+import ru.geekbrains.coursework.webshopclouduirestdao.representations.ProductRep;
 
 @Service
-public class ProductService extends AService<Product, ProductRepository> {
+public class ProductService extends AService<Product, ProductRep, ProductRepository> {
+    @Override
+    public void save(ProductRep representation) {
+
+    }
 }
