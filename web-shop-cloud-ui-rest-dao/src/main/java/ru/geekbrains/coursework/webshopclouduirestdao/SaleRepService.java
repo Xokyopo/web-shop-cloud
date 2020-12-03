a82321ce2a8dcc8faf779090e8e73b84dca47e42
Alexey <enslaver@list.ru>
2020-12-03 18:27:34 +0300

    removed return value in method with name "sale"

--- web-shop-cloud-ui-rest-dao/src/main/java/ru/geekbrains/coursework/webshopclouduirestdao/SaleRepService.java
+++ web-shop-cloud-ui-rest-dao/src/main/java/ru/geekbrains/coursework/webshopclouduirestdao/SaleRepService.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 public interface SaleRepService {
     @PostMapping("/sale")
-    List<SaleRep> sale(@RequestParam("cart") HashMap<ProductRep, Integer> cart);
+    void sale(@RequestParam("cart") HashMap<ProductRep, Integer> cart);
 
     @GetMapping("/getAll")
     List<SaleRep> getAll();
