811cfb10a8d2fa2f4552b4918982eddb1de486da
Alexey <enslaver@list.ru>
2020-12-03 18:21:46 +0300

    added Sale Rest controller

---
+++ web-shop-cloud-domain/src/main/java/ru/geekbrains/coursework/webshopclouddomain/app/ui/rest/SaleRepController.java
@@ -0,0 +1,42 @@
+package ru.geekbrains.coursework.webshopclouddomain.app.ui.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.geekbrains.coursework.webshopclouddomain.app.domain.SaleService;
+import ru.geekbrains.coursework.webshopclouduirestdao.SaleRepService;
+import ru.geekbrains.coursework.webshopclouduirestdao.representations.ProductRep;
+import ru.geekbrains.coursework.webshopclouduirestdao.representations.SaleRep;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/sale")
+public class SaleRepController implements SaleRepService {
+    private SaleService service;
+
+    @Autowired
+    public SaleRepController(SaleService service) {
+        this.service = service;
+    }
+
+    @Override
+    @PostMapping("/sale")
+    public void sale(@RequestParam("cart") HashMap<ProductRep, Integer> cart) {
+        this.service.sale(cart);
+    }
+
+    @Override
+    @GetMapping("/getAll")
+    public List<SaleRep> getAll() {
+        return this.service.getAll();
+    }
+
+    @Override
+    @GetMapping("/getProductBySaleId/{id}")
+    public Optional<ProductRep> getProductBySaleId(@PathVariable("id") long id) {
+        return this.service.getProductBySaleId(id);
+    }
+
+}
