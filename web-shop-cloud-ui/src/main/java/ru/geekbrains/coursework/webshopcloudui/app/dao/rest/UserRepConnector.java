d134f436e5e9270883f0dbaf94beb7a0b4afabf3
Alexey <enslaver@list.ru>
2020-12-04 00:16:21 +0300

    added method annotation

--- web-shop-cloud-ui/src/main/java/ru/geekbrains/coursework/webshopcloudui/app/dao/rest/UserRepConnector.java
+++ web-shop-cloud-ui/src/main/java/ru/geekbrains/coursework/webshopcloudui/app/dao/rest/UserRepConnector.java
@@ -1,10 +1,13 @@
 package ru.geekbrains.coursework.webshopcloudui.app.dao.rest;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.geekbrains.coursework.webshopclouduirestdao.ARemoteService;
 import ru.geekbrains.coursework.webshopclouduirestdao.representations.UserRep;
 
 @FeignClient(value = "user-service", url = "http://localhost:9090/user")
 public interface UserRepConnector extends ARemoteService<UserRep> {
+
+    @PostMapping("/addNewUser")
     void addNewUser(String login, String password) throws Exception;
 }
