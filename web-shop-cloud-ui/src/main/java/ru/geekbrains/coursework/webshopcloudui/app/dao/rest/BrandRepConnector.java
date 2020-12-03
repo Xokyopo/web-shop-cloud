3e28ecb66c888893fa3e10d3e124c54baf582abd
Alexey <enslaver@list.ru>
2020-12-03 23:09:04 +0300

    fixed add import for "ARemoteService" interface

--- web-shop-cloud-ui/src/main/java/ru/geekbrains/coursework/webshopcloudui/app/dao/rest/BrandRepConnector.java
+++ web-shop-cloud-ui/src/main/java/ru/geekbrains/coursework/webshopcloudui/app/dao/rest/BrandRepConnector.java
@@ -1,6 +1,7 @@
 package ru.geekbrains.coursework.webshopcloudui.app.dao.rest;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import ru.geekbrains.coursework.webshopclouduirestdao.ARemoteService;
 import ru.geekbrains.coursework.webshopclouduirestdao.representations.BrandRep;
 
 @FeignClient(value = "brand-service", url = "http://localhost:9090/brand")
