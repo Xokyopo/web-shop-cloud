12b9d07b35462a034209165c2373dfb984cfae18
Alexey <enslaver@list.ru>
2020-12-06 16:37:01 +0300

    configured eureka client for ru.geekbrains.coursework.webshopclouddomain module

--- web-shop-cloud-domain/src/main/java/ru/geekbrains/coursework/webshopclouddomain/WebShopCloudDomainApplication.java
+++ web-shop-cloud-domain/src/main/java/ru/geekbrains/coursework/webshopclouddomain/WebShopCloudDomainApplication.java
@@ -2,10 +2,12 @@ package ru.geekbrains.coursework.webshopclouddomain;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
+@EnableEurekaClient
 public class WebShopCloudDomainApplication {
 
 	public static void main(String[] args) {
