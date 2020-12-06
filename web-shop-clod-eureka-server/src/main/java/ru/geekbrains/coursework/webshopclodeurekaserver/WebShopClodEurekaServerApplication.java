7775fbf5e508b958c9019fd9ac1a1a68c21c7b1c
Alexey <enslaver@list.ru>
2020-12-06 15:20:37 +0300

    added web-shop-clod-eureka-server module

---
+++ web-shop-clod-eureka-server/src/main/java/ru/geekbrains/coursework/webshopclodeurekaserver/WebShopClodEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.coursework.webshopclodeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopClodEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebShopClodEurekaServerApplication.class, args);
+    }
+
+}
---
+++ web-shop-clod-eureka-server/src/test/java/ru/geekbrains/coursework/webshopclodeurekaserver/WebShopClodEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.coursework.webshopclodeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopClodEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
