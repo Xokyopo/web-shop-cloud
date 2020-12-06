e3791cd13797fe9716427db38f3ad9b03c9f9663
Alexey <enslaver@list.ru>
2020-12-06 15:49:13 +0300

    configured eureka server

--- web-shop-clod-eureka-server/src/main/java/ru/geekbrains/coursework/webshopclodeurekaserver/WebShopClodEurekaServerApplication.java
+++ web-shop-clod-eureka-server/src/main/java/ru/geekbrains/coursework/webshopclodeurekaserver/WebShopClodEurekaServerApplication.java
@@ -2,8 +2,10 @@ package ru.geekbrains.coursework.webshopclodeurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class WebShopClodEurekaServerApplication {
 
     public static void main(String[] args) {
