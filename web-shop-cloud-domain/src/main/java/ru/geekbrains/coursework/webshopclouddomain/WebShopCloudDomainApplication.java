0795c0633d68fa3303e731de36a07cd947cb652d
Alexey <enslaver@list.ru>
2020-12-07 12:59:05 +0300

    disabled AOP annotation

--- web-shop-cloud-domain/src/main/java/ru/geekbrains/coursework/webshopclouddomain/WebShopCloudDomainApplication.java
+++ web-shop-cloud-domain/src/main/java/ru/geekbrains/coursework/webshopclouddomain/WebShopCloudDomainApplication.java
@@ -3,9 +3,7 @@ package ru.geekbrains.coursework.webshopclouddomain;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@EnableAspectJAutoProxy
 @SpringBootApplication
 @EnableEurekaClient
 public class WebShopCloudDomainApplication {
